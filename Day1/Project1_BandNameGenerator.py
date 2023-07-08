7a3c6fdfe02f9bfa682f472c93a1648202872552
Alister D'souza <alister@chara.co.in>
2023-07-08 11:55:17 +0530

    Day 1 of Hundred Days Of Code.

---
+++ Day1/Project1_BandNameGenerator.py
@@ -0,0 +1,17 @@
+# Greeting
+print("Welcome to the Band Name Generator.")
+
+# Asking for name of city from user
+print("What's the city you grew up in? \n")
+city_name =(input())
+
+# Asking for name of pet from user
+print("What's your pet's name? \n")
+pet_name = (input())
+
+
+# Output of Band Name
+
+print("Your band name could be --> " + city_name + " " + pet_name)
+
+
---
+++ Day1/stringConcat.py
@@ -0,0 +1,1 @@
+print("Alister" + " " + "D'souza")

9af9cd141d61a6e1269d2fab06930989fe299b2e
Alister D'souza <alister@chara.co.in>
2023-07-09 19:19:59 +0530

    Day 2 of Hundred Days Of Code.

---
+++ Day2/Project2_TipCalculator.py
@@ -0,0 +1,16 @@
+# Tip Calculator Project
+
+print("Welcome to the tip calculator \n")
+
+total_bill = input(print("What was the total bill ? $ "))
+total_bill = float(total_bill)
+
+percentage_tip = int(input(print("What percentage of tip would you like to give? 10, 12 or 15. \n")))
+percentage_tip = ((total_bill * percentage_tip) / 100)
+
+numOfPeople = int(input(print("How many people you want to split the bill with? \n")))
+
+total_bill_per_head = (total_bill + percentage_tip) / numOfPeople
+total_bill_per_head = round(total_bill_per_head, 2)
+
+print(f"Your bill per person is ${total_bill_per_head}")

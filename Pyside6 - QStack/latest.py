5033ac981321ffb4893e98393118cc62e0564d72
alisterdsouzaa <alister@chara.co.in>
2023-09-05 09:55:41 +0530

    Ordered / Unordered List Tags

--- Pyside6 - QStack/latest.py
+++ Pyside6 - QStack/latest.py
@@ -7,9 +7,6 @@ count_list = []
 zeroth_column_values = []
 first_column_values = []
 second_column_values = []
-i = -1  # Values in Description
-j = -1  # Values in Spin box ( Decimal values )
-
 
 class stackedExample(QWidget):
     def __init__(self):
@@ -31,8 +28,6 @@ class stackedExample(QWidget):
                            ]  # Add more item names if needed
 
         self.stack_layouts = {}  # Dictionary to store stack layouts
-
-        self.initial_load_and_count_csv()
 
         for item_name in self.item_names:
             stack_layout = QWidget()
@@ -64,16 +59,8 @@ class stackedExample(QWidget):
         button_layout.addWidget(self.leftlist)
         button_layout.addWidget(self.Stack)
 
-        # hbox = QHBoxLayout(self)
-        # hbox.addWidget(self.leftlist)
-        # hbox.addWidget(self.Stack)
-        # hbox.addWidget(self.save_csv_button)
-        # hbox.addWidget(self.open_csv_button)
-        # hbox.addWidget(self.extract_values_button)
-
         vbox = QVBoxLayout()
         vbox.addLayout(button_layout)
-        # vbox.addLayout(hbox)
 
         self.setLayout(vbox)
         self.leftlist.currentRowChanged.connect(self.display)
@@ -87,13 +74,9 @@ class stackedExample(QWidget):
         global first_column_values
 
         count_list = []
-        # print(count_list)
         second_column_values = []
-        print(second_column_values)
-        # zeroth_column_values = []
-        print(zeroth_column_values)
-        # first_column_values = []
-        print(first_column_values)
+        zeroth_column_values = []
+        first_column_values = []
 
         options = QFileDialog.Options()
         options |= QFileDialog.ReadOnly
@@ -122,99 +105,17 @@ class stackedExample(QWidget):
                     number_counts[num] = 1
 
             count_list = [number_counts.get(i, 0) for i in range(11)]
-            # print(int_addresses)
-            print(count_list)
-            print(zeroth_column_values)
-            print(first_column_values)
-            print(second_column_values)
 
-            global i
-            i = -1
-            global j
-            j = -1
+            self.updateStackLayouts()
 
-            for item_name in self.item_names:
-                stack_layout = QWidget()
-                self.createStackLayout(stack_layout, item_name)
-                self.stack_layouts[item_name] = stack_layout
-                self.leftlist.addItem(item_name)
+    def updateStackLayouts(self):
+        global i
+        i = -1
+        global j
+        j = -1
 
-            self.Stack = QStackedWidget(self)
-            for item_name, stack_layout in self.stack_layouts.items():
-                scroll_area = QScrollArea()  # Create a QScrollArea
-                scroll_area.setWidgetResizable(True)
-                scroll_area.setWidget(stack_layout)
-                self.Stack.addWidget(scroll_area)
-
-            self.open_csv_button = QPushButton("Open CSV")
-            self.open_csv_button.clicked.connect(self.load_and_count_csv)
-
-            self.save_csv_button = QPushButton("Save CSV")
-            self.save_csv_button.clicked.connect(self.dummyfunc)
-
-            self.extract_values_button = QPushButton("Extract Values")
-            self.extract_values_button.clicked.connect(self.extract_values)
-
-            button_layout = QHBoxLayout()
-            button_layout.addWidget(self.open_csv_button)
-            button_layout.addWidget(self.save_csv_button)
-            button_layout.addWidget(self.extract_values_button)  # Add the new button
-
-            button_layout.addWidget(self.leftlist)
-            button_layout.addWidget(self.Stack)
-
-            # hbox = QHBoxLayout(self)
-            # hbox.addWidget(self.leftlist)
-            # hbox.addWidget(self.Stack)
-            # hbox.addWidget(self.save_csv_button)
-            # hbox.addWidget(self.open_csv_button)
-            # hbox.addWidget(self.extract_values_button)
-
-            vbox = QVBoxLayout()
-            vbox.addLayout(button_layout)
-            # vbox.addLayout(hbox)
-
-            self.setLayout(vbox)
-            self.leftlist.currentRowChanged.connect(self.display)
-            self.setWindowTitle('StackedWidget demo')
-            self.show()
-
-    def initial_load_and_count_csv(self):
-        global count_list
-        global second_column_values
-        global zeroth_column_values
-        global first_column_values
-
-        file_path = "v3.0.0 - Akash_m2 (1) (2).csv"
-
-        if file_path:
-            with open(file_path, 'r') as csv_file:
-                csv_reader = csv.reader(csv_file)
-                for row in csv_reader:
-                    zeroth_column_values.append(row[0])
-                    first_column_values.append(row[1])
-                    second_column_values.append(row[2])
-
-            zeroth_column_values = zeroth_column_values[1:]
-            first_column_values = first_column_values[1:]
-            second_column_values = second_column_values[1:]
-
-            int_addresses = [int(hex_address, 16) >> 8 for hex_address in zeroth_column_values]
-
-            number_counts = {}
-
-            for num in int_addresses:
-                if num in number_counts:
-                    number_counts[num] += 1
-                else:
-                    number_counts[num] = 1
-
-            count_list = [number_counts.get(i, 0) for i in range(11)]
-            # print(int_addresses)
-            print(count_list)
-            print(zeroth_column_values)
-            print(first_column_values)
-            print(second_column_values)
+        for item_name in self.item_names:
+            self.createStackLayout(self.stack_layouts[item_name], item_name)
 
     def createStackLayout(self, stack_layout, item_name):
         layout = QVBoxLayout()
@@ -261,27 +162,23 @@ class stackedExample(QWidget):
             for _ in range(count_list[self.item_names.index(item_name)]):
                 stack_layout = self.stack_layouts[item_name].layout()
                 if stack_layout is not None:
-                    # Traverse the layout and find QSpinBox widgets
-                    for index in range(stack_layout.count()):
-                        item_at = stack_layout.itemAt(index)
-                        if item_at and isinstance(item_at.widget(), QSpinBox):
-                            spin_box_value = item_at.widget().value()
-                            extracted_values.append(spin_box_value)
-                            jj += 1
-                        else:
-                            break
+                    item_at = stack_layout.itemAt(jj + 2)
+                    if item_at is not None and isinstance(item_at.widget(), QSpinBox):
+                        spin_box_value = item_at.widget().value()
+                        extracted_values.append(spin_box_value)
+                        jj += 1
+                    else:
+                        break
                 else:
                     break
 
         print("Extracted Values:", extracted_values)
-
 
 def main():
     app = QApplication(sys.argv)
     ex = stackedExample()
     ex.showMaximized()
     sys.exit(app.exec())
-
 
 if __name__ == '__main__':
     main()
